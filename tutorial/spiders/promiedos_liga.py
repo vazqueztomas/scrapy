c11419d0dc07808ad1ec560b30d4d5cd4b37a350
Tomas Vazquez <vazquezt2018@gmail.com>
2023-03-22 13:02:01 -0300

    scrapy realized, scrapping promiedos.com

---
+++ tutorial/spiders/promiedos_liga.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import scrapy
+
+
+class LigaSpider(scrapy.Spider):
+    name = 'liga_argentina'
+
+    def start_requests(self):
+        urls = ['https://www.promiedos.com.ar/primera',
+                "https://www.promiedos.com.ar/primera=equipos"]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'{page}.html'
+        Path(filename).write_bytes(response.body)
+        self.log(f'Saved file {filename}')
+
+        for equipo in response.css('div.eqs strong'):
+            yield {
+                'equipo': equipo.css('strong').get(),
+            }
+
+        next_page = "/" + response.css('ul.items-menu li a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
